9ca17d937a4ce2842ac25f4960422fd4b945f333
Jamie Pinnington <JP878@canterbury.ac.uk>
2022-12-06 15:58:03 +0000

    This is my first CI container.

---
+++ test_example.py
@@ -0,0 +1,12 @@
+from example import *
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -2) == -1
+    assert add(1000000000000000000, 2) == 1000000000000000002
+
+def test_multiply():
+    assert multiply(1, 2) == 2
+    assert multiply(2, 4) == 8
+    assert multiply(5, 3) == 15
+    assert multiply(-2, -2) == 4
